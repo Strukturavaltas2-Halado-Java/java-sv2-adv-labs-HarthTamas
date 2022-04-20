aee62c92ff7667aedf7b06339e5cca9267222684
ht0m <hartht@gmail.com>
2022-04-20 16:49:34 +0200

    formatted

--- labs-solutions/webshop/src/main/java/webshop/WebShopMain.java
+++ labs-solutions/webshop/src/main/java/webshop/WebShopMain.java
@@ -73,11 +73,11 @@ public class WebShopMain {
         messagePrint("Regisztráció");
         List<String> input = inputsToList("Felhasználónév:", "Jelszó:", "Email:");
         try {
-        shopService.registerUser(input.get(0), input.get(1), input.get(2));
-        messagePrint("Helló " + input.get(0) + ", a regisztráció sikeres volt.");
-        messagePrint("Extra Kedvezményekért iratkozz fel hírlevelünkre is!");
+            shopService.registerUser(input.get(0), input.get(1), input.get(2));
+            messagePrint("Helló " + input.get(0) + ", a regisztráció sikeres volt.");
+            messagePrint("Extra Kedvezményekért iratkozz fel hírlevelünkre is!");
         } catch (IllegalArgumentException iae) {
-            messagePrint("Hiba a regisztáció során: "+iae.getMessage());
+            messagePrint("Hiba a regisztáció során: " + iae.getMessage());
         }
     }
 
@@ -85,8 +85,8 @@ public class WebShopMain {
         messagePrint("Belépés");
         List<String> input = inputsToList("Felhasználónév:", "Jelszó:");
         try {
-        shopService.logIn(input.get(0), input.get(1));
-        runShoppingMenu();
+            shopService.logIn(input.get(0), input.get(1));
+            runShoppingMenu();
         } catch (IllegalArgumentException iae) {
             messagePrint(iae.getMessage());
         }
